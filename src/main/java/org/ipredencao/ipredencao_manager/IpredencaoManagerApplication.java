2315593561a1e5dd4489c8d29739f0144382acf3
Luiz Barboza <lbarboza@metropolis.io>
2025-07-08 10:49:12 -0300

    First commit

---
+++ src/main/java/org/ipredencao/ipredencao_manager/IpredencaoManagerApplication.java
@@ -0,0 +1,13 @@
+package org.ipredencao.ipredencao_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IpredencaoManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IpredencaoManagerApplication.class, args);
+	}
+
+}
---
+++ src/main/resources/db/changelog/V001__2025-07-08_tables.sql
@@ -0,0 +1,44 @@
+CREATE TYPE estado_civil AS ENUM ('SOLTEIRO', 'CASADO', 'DIVORCIADO', 'VIUVO', 'SEPARADO');
+CREATE TYPE tipo_batismo AS ENUM ('INFANTIL', 'ADULTO', 'NAO_BATIZADO');
+CREATE TYPE estado_pessoa AS ENUM ('ATIVO', 'INATIVO', 'FALECIDO', 'TRANSFERIDO');
+CREATE TYPE tipo_admissao AS ENUM ('BATISMO', 'PROFISSAO_DE_FE', 'TRANSFERENCIA', 'OUTROS');
+
+CREATE TABLE IF NOT EXISTS pessoa (
+    id BIGSERIAL PRIMARY KEY,
+    nome VARCHAR(255),
+    sede_congregacao VARCHAR(255),
+    apelido VARCHAR(255),
+    data_nascimento TIMESTAMP,
+    telefone VARCHAR(30),
+    estado_civil estado_civil,
+    igreja_anterior VARCHAR(255),
+    situacao_igreja_anterior VARCHAR(255),
+    tempo_na_ipr VARCHAR(100),
+    motivos_admissao VARCHAR(255),
+    tipo_batismo tipo_batismo,
+    data_batismo TIMESTAMP,
+    igreja_batismo VARCHAR(255),
+    dados_oficial VARCHAR(255),
+    supervisoes VARCHAR(255),
+    outras_categorias VARCHAR(255),
+    estado_pessoa estado_pessoa,
+    tipo_admissao tipo_admissao,
+    cpf VARCHAR(20),
+    rg VARCHAR(20),
+    email_adicional VARCHAR(255),
+    telefone_adicional VARCHAR(30),
+    email_trabalho VARCHAR(255),
+    telefone_trabalho VARCHAR(30),
+    profissao VARCHAR(100),
+    empresa VARCHAR(100),
+    endereco VARCHAR(255),
+    latitude NUMERIC(10,8),
+    longitude NUMERIC(11,8),
+    regiao_gf VARCHAR(100),
+    skype VARCHAR(100),
+    twitter VARCHAR(100),
+    linkedin VARCHAR(100),
+    instagram VARCHAR(100),
+    facebook VARCHAR(100),
+    pagina_pessoal VARCHAR(255)
+); 
