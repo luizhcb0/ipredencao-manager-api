c11a46a49ec5a5380ccdc9e76ca51ea532037498
Samuel Couto <samuelcoouto@hotmail.com>
2025-07-25 23:23:59 -0300

    feat: CRUD para FormularioPessoa

---
+++ src/main/java/org/ipredencao/ipredencao_manager/model/FormularioPessoa.java
@@ -0,0 +1,118 @@
+package org.ipredencao.ipredencao_manager.model;
+
+import org.joda.time.DateTime;
+import java.util.List;
+
+public class FormularioPessoa {
+    private Long id;
+    private String nome;
+    private String apelido;
+    private String email;
+    private List<String> emailsSecundarios;
+    private String telefone;
+    private List<String> telefonesSecundarios;
+    private String campus;
+    private DateTime dataNascimento;
+    private String cpf;
+    private String rg;
+    private EstadoCivil estadoCivil;
+    private String igrejaAnterior;
+    private String situacaoIgrejaAnterior;
+    private String tempoNaIgreja;
+    private String motivosParaAdmissao;
+    private TipoBatismo tipoBatismo;
+    private DateTime dataBatismo;
+    private DateTime dataProfissaoDeFe;
+    private String igrejaBatismo;
+    private String profissao;
+    private String empresa;
+    private String endereco;
+    private Regiao regiao;
+    private Double latitude;
+    private Double longitude;
+    private String fotoUrl;
+    private Sexo sexo;
+    private Long chefeDeFamiliaId;
+    private SubcategoriaEnum subcategoria;
+    // Campos extras do formulário
+    private String nomeParceiro;
+    private String nomePai;
+    private String nomeMae;
+    private List<String> nomeFilhos;
+    private DateTime addedAt;
+    private DateTime updatedAt;
+
+    // Getters e setters
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
+    public String getApelido() { return apelido; }
+    public void setApelido(String apelido) { this.apelido = apelido; }
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+    public List<String> getEmailsSecundarios() { return emailsSecundarios; }
+    public void setEmailsSecundarios(List<String> emailsSecundarios) { this.emailsSecundarios = emailsSecundarios; }
+    public String getTelefone() { return telefone; }
+    public void setTelefone(String telefone) { this.telefone = telefone; }
+    public List<String> getTelefonesSecundarios() { return telefonesSecundarios; }
+    public void setTelefonesSecundarios(List<String> telefonesSecundarios) { this.telefonesSecundarios = telefonesSecundarios; }
+    public String getCampus() { return campus; }
+    public void setCampus(String campus) { this.campus = campus; }
+    public DateTime getDataNascimento() { return dataNascimento; }
+    public void setDataNascimento(DateTime dataNascimento) { this.dataNascimento = dataNascimento; }
+    public String getCpf() { return cpf; }
+    public void setCpf(String cpf) { this.cpf = cpf; }
+    public String getRg() { return rg; }
+    public void setRg(String rg) { this.rg = rg; }
+    public EstadoCivil getEstadoCivil() { return estadoCivil; }
+    public void setEstadoCivil(EstadoCivil estadoCivil) { this.estadoCivil = estadoCivil; }
+    public String getIgrejaAnterior() { return igrejaAnterior; }
+    public void setIgrejaAnterior(String igrejaAnterior) { this.igrejaAnterior = igrejaAnterior; }
+    public String getSituacaoIgrejaAnterior() { return situacaoIgrejaAnterior; }
+    public void setSituacaoIgrejaAnterior(String situacaoIgrejaAnterior) { this.situacaoIgrejaAnterior = situacaoIgrejaAnterior; }
+    public String getTempoNaIgreja() { return tempoNaIgreja; }
+    public void setTempoNaIgreja(String tempoNaIgreja) { this.tempoNaIgreja = tempoNaIgreja; }
+    public String getMotivosParaAdmissao() { return motivosParaAdmissao; }
+    public void setMotivosParaAdmissao(String motivosParaAdmissao) { this.motivosParaAdmissao = motivosParaAdmissao; }
+    public TipoBatismo getTipoBatismo() { return tipoBatismo; }
+    public void setTipoBatismo(TipoBatismo tipoBatismo) { this.tipoBatismo = tipoBatismo; }
+    public DateTime getDataBatismo() { return dataBatismo; }
+    public void setDataBatismo(DateTime dataBatismo) { this.dataBatismo = dataBatismo; }
+    public DateTime getDataProfissaoDeFe() { return dataProfissaoDeFe; }
+    public void setDataProfissaoDeFe(DateTime dataProfissaoDeFe) { this.dataProfissaoDeFe = dataProfissaoDeFe; }
+    public String getIgrejaBatismo() { return igrejaBatismo; }
+    public void setIgrejaBatismo(String igrejaBatismo) { this.igrejaBatismo = igrejaBatismo; }
+    public String getProfissao() { return profissao; }
+    public void setProfissao(String profissao) { this.profissao = profissao; }
+    public String getEmpresa() { return empresa; }
+    public void setEmpresa(String empresa) { this.empresa = empresa; }
+    public String getEndereco() { return endereco; }
+    public void setEndereco(String endereco) { this.endereco = endereco; }
+    public Regiao getRegiao() { return regiao; }
+    public void setRegiao(Regiao regiao) { this.regiao = regiao; }
+    public Double getLatitude() { return latitude; }
+    public void setLatitude(Double latitude) { this.latitude = latitude; }
+    public Double getLongitude() { return longitude; }
+    public void setLongitude(Double longitude) { this.longitude = longitude; }
+    public String getFotoUrl() { return fotoUrl; }
+    public void setFotoUrl(String fotoUrl) { this.fotoUrl = fotoUrl; }
+    public Sexo getSexo() { return sexo; }
+    public void setSexo(Sexo sexo) { this.sexo = sexo; }
+    public Long getChefeDeFamiliaId() { return chefeDeFamiliaId; }
+    public void setChefeDeFamiliaId(Long chefeDeFamiliaId) { this.chefeDeFamiliaId = chefeDeFamiliaId; }
+    public SubcategoriaEnum getSubcategoria() { return subcategoria; }
+    public void setSubcategoria(SubcategoriaEnum subcategoria) { this.subcategoria = subcategoria; }
+    public String getNomeParceiro() { return nomeParceiro; }
+    public void setNomeParceiro(String nomeParceiro) { this.nomeParceiro = nomeParceiro; }
+    public String getNomePai() { return nomePai; }
+    public void setNomePai(String nomePai) { this.nomePai = nomePai; }
+    public String getNomeMae() { return nomeMae; }
+    public void setNomeMae(String nomeMae) { this.nomeMae = nomeMae; }
+    public List<String> getNomeFilhos() { return nomeFilhos; }
+    public void setNomeFilhos(List<String> nomeFilhos) { this.nomeFilhos = nomeFilhos; }
+    public DateTime getAddedAt() { return addedAt; }
+    public void setAddedAt(DateTime addedAt) { this.addedAt = addedAt; }
+    public DateTime getUpdatedAt() { return updatedAt; }
+    public void setUpdatedAt(DateTime updatedAt) { this.updatedAt = updatedAt; }
+} 
---
+++ src/main/java/org/ipredencao/ipredencao_manager/repository/FormularioPessoaRepository.java
@@ -0,0 +1,163 @@
+package org.ipredencao.ipredencao_manager.repository;
+
+import org.ipredencao.ipredencao_manager.model.FormularioPessoa;
+import org.ipredencao.ipredencao_manager.model.Pessoa;
+import org.ipredencao.ipredencao_manager.util.DateTimeHelper;
+import org.jooq.DSLContext;
+import org.jooq.Record;
+import org.jooq.impl.DSL;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.joda.time.DateTime;
+
+// Importe os records e tabelas do JOOQ gerados para formulario_pessoa
+import static org.ipredencao.ipredencao_manager.jooq.tables.FormularioPessoa.FORMULARIO_PESSOA;
+import org.ipredencao.ipredencao_manager.jooq.tables.records.FormularioPessoaRecord;
+
+@Repository
+public class FormularioPessoaRepository {
+    @Autowired
+    private DSLContext dsl;
+
+    public FormularioPessoa insert(FormularioPessoa formulario) {
+        FormularioPessoaRecord record = toRepository(formulario);
+        FormularioPessoaRecord saved = dsl.insertInto(FORMULARIO_PESSOA)
+                .set(record)
+                .returning()
+                .fetchOne();
+        return fromRepository(saved);
+    }
+
+    public FormularioPessoa update(FormularioPessoa formulario) {
+        FormularioPessoaRecord record = toRepository(formulario);
+        FormularioPessoaRecord updated = dsl.update(FORMULARIO_PESSOA)
+                .set(record)
+                .set(FORMULARIO_PESSOA.UPDATED_AT, DateTimeHelper.toDb(DateTime.now()))
+                .where(FORMULARIO_PESSOA.FORMULARIO_PESSOA_ID.eq(formulario.getId()))
+                .returning()
+                .fetchOne();
+        return fromRepository(updated);
+    }
+
+    public List<FormularioPessoa> findAll() {
+        return dsl.selectFrom(FORMULARIO_PESSOA)
+                .fetch()
+                .stream()
+                .map(FormularioPessoaRepository::fromRepository)
+                .collect(Collectors.toList());
+    }
+
+    public FormularioPessoa findById(Long id) {
+        FormularioPessoaRecord record = dsl.selectFrom(FORMULARIO_PESSOA)
+                .where(FORMULARIO_PESSOA.FORMULARIO_PESSOA_ID.eq(id))
+                .fetchOne();
+        return fromRepository(record);
+    }
+
+    private static FormularioPessoa fromRepository(FormularioPessoaRecord record) {
+        if (record == null) return null;
+        FormularioPessoa f = new FormularioPessoa();
+        f.setId(record.getFormularioPessoaId());
+        f.setNome(record.getNome());
+        f.setApelido(record.getApelido());
+        f.setEmail(record.getEmail());
+        f.setTelefone(record.getTelefone());
+        f.setCampus(record.getCampus());
+        f.setDataNascimento(DateTimeHelper.fromDb(record.getDataNascimento()));
+        f.setCpf(record.getCpf());
+        f.setRg(record.getRg());
+        if (record.getEstadoCivil() != null)
+            f.setEstadoCivil(org.ipredencao.ipredencao_manager.model.EstadoCivil.valueOf(record.getEstadoCivil().name()));
+        f.setIgrejaAnterior(record.getIgrejaAnterior());
+        f.setSituacaoIgrejaAnterior(record.getSituacaoIgrejaAnterior());
+        f.setTempoNaIgreja(record.getTempoNaIgreja());
+        f.setMotivosParaAdmissao(record.getMotivosParaAdmissao());
+        if (record.getTipoBatismo() != null)
+            f.setTipoBatismo(org.ipredencao.ipredencao_manager.model.TipoBatismo.valueOf(record.getTipoBatismo().name()));
+        f.setDataBatismo(DateTimeHelper.fromDb(record.getDataBatismo()));
+        f.setDataProfissaoDeFe(DateTimeHelper.fromDb(record.getDataProfissaoDeFe()));
+        f.setIgrejaBatismo(record.getIgrejaBatismo());
+        f.setProfissao(record.getProfissao());
+        f.setEmpresa(record.getEmpresa());
+        f.setEndereco(record.getEndereco());
+        if (record.getRegiao() != null)
+            f.setRegiao(org.ipredencao.ipredencao_manager.model.Regiao.valueOf(record.getRegiao().name()));
+        f.setLatitude(record.getLatitude() != null ? record.getLatitude().doubleValue() : null);
+        f.setLongitude(record.getLongitude() != null ? record.getLongitude().doubleValue() : null);
+        f.setFotoUrl(record.getFotoUrl());
+        if (record.getSexo() != null)
+            f.setSexo(org.ipredencao.ipredencao_manager.model.Sexo.valueOf(record.getSexo().name()));
+        // chefeDeFamilia: no formulário é String, na model é Long. Não mapeia diretamente.
+        if (record.getCategoriaId() != null)
+            f.setSubcategoria(org.ipredencao.ipredencao_manager.model.SubcategoriaEnum.fromId(record.getCategoriaId()));
+        if (record.getEmailsSecundarios() != null) {
+            f.setEmailsSecundarios(java.util.Arrays.asList(record.getEmailsSecundarios()));
+        }
+        if (record.getTelefonesSecundarios() != null) {
+            f.setTelefonesSecundarios(java.util.Arrays.asList(record.getTelefonesSecundarios()));
+        }
+        // Campos extras do formulário
+        f.setNomeParceiro(record.getNomeParceiro());
+        f.setNomePai(record.getNomePai());
+        f.setNomeMae(record.getNomeMae());
+        f.setNomeFilhos(record.getNomeFilhos() != null ? java.util.Arrays.asList(record.getNomeFilhos()) : null);
+        f.setAddedAt(DateTimeHelper.fromDb(record.getAddedAt()));
+        f.setUpdatedAt(DateTimeHelper.fromDb(record.getUpdatedAt()));
+        return f;
+    }
+
+    private static FormularioPessoaRecord toRepository(FormularioPessoa f) {
+        FormularioPessoaRecord record = new FormularioPessoaRecord();
+        if (f.getId() != null) record.setFormularioPessoaId(f.getId());
+        record.setNome(f.getNome());
+        record.setApelido(f.getApelido());
+        record.setEmail(f.getEmail());
+        record.setTelefone(f.getTelefone());
+        record.setCampus(f.getCampus());
+        record.setDataNascimento(DateTimeHelper.toDb(f.getDataNascimento()));
+        record.setCpf(f.getCpf());
+        record.setRg(f.getRg());
+        if (f.getEstadoCivil() != null)
+            record.setEstadoCivil(org.ipredencao.ipredencao_manager.jooq.enums.EstadoCivil.valueOf(f.getEstadoCivil().name()));
+        record.setIgrejaAnterior(f.getIgrejaAnterior());
+        record.setSituacaoIgrejaAnterior(f.getSituacaoIgrejaAnterior());
+        record.setTempoNaIgreja(f.getTempoNaIgreja());
+        record.setMotivosParaAdmissao(f.getMotivosParaAdmissao());
+        if (f.getTipoBatismo() != null)
+            record.setTipoBatismo(org.ipredencao.ipredencao_manager.jooq.enums.TipoBatismo.valueOf(f.getTipoBatismo().name()));
+        record.setDataBatismo(DateTimeHelper.toDb(f.getDataBatismo()));
+        record.setDataProfissaoDeFe(DateTimeHelper.toDb(f.getDataProfissaoDeFe()));
+        record.setIgrejaBatismo(f.getIgrejaBatismo());
+        record.setProfissao(f.getProfissao());
+        record.setEmpresa(f.getEmpresa());
+        record.setEndereco(f.getEndereco());
+        if (f.getRegiao() != null)
+            record.setRegiao(org.ipredencao.ipredencao_manager.jooq.enums.Regiao.valueOf(f.getRegiao().name()));
+        if (f.getLatitude() != null) record.setLatitude(java.math.BigDecimal.valueOf(f.getLatitude()));
+        if (f.getLongitude() != null) record.setLongitude(java.math.BigDecimal.valueOf(f.getLongitude()));
+        record.setFotoUrl(f.getFotoUrl());
+        if (f.getSexo() != null)
+            record.setSexo(org.ipredencao.ipredencao_manager.jooq.enums.Sexo.valueOf(f.getSexo().name()));
+        // chefeDeFamilia: no formulário é String, na model é Long. Não mapeia diretamente.
+        if (f.getSubcategoria() != null) {
+            record.setCategoriaId(f.getSubcategoria().getId());
+        }
+        if (f.getEmailsSecundarios() != null && !f.getEmailsSecundarios().isEmpty()) {
+            record.setEmailsSecundarios(f.getEmailsSecundarios().toArray(new String[0]));
+        }
+        if (f.getTelefonesSecundarios() != null && !f.getTelefonesSecundarios().isEmpty()) {
+            record.setTelefonesSecundarios(f.getTelefonesSecundarios().toArray(new String[0]));
+        }
+        // Campos extras do formulário
+        record.setNomeParceiro(f.getNomeParceiro());
+        record.setNomePai(f.getNomePai());
+        record.setNomeMae(f.getNomeMae());
+        if (f.getNomeFilhos() != null)
+            record.setNomeFilhos(f.getNomeFilhos().toArray(new String[0]));
+        record.setAddedAt(DateTimeHelper.toDb(f.getAddedAt()));
+        record.setUpdatedAt(DateTimeHelper.toDb(f.getUpdatedAt()));
+        return record;
+    }
+} 
