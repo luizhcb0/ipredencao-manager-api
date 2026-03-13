6bdc1a80445e9c252983e8f843433923ba48395c
Luiz Barboza <lbarboza@metropolis.io>
2026-03-13 11:57:35 -0300

    aws ready

---
+++ src/main/java/org/ipredencao/ipredencao_manager/service/firebase/VerifiedToken.java
@@ -0,0 +1,11 @@
+package org.ipredencao.ipredencao_manager.service.firebase;
+
+/**
+ * Token Firebase verificado - substitui FirebaseToken que é final
+ */
+public record VerifiedToken(
+    String uid,
+    String email,
+    String name,
+    boolean emailVerified
+) {}
