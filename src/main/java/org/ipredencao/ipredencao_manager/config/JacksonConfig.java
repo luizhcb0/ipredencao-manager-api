f56ee9c44bf4f6b803ebf956e56213cbadff4758
Luiz Barboza <lbarboza@metropolis.io>
2026-03-18 19:54:34 -0300

    Better error handling

--- src/main/java/org/ipredencao/ipredencao_manager/config/JacksonConfig.java
+++ src/main/java/org/ipredencao/ipredencao_manager/config/JacksonConfig.java
@@ -1,16 +1,14 @@
 package org.ipredencao.ipredencao_manager.config;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.joda.JodaModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class JacksonConfig {
+
     @Bean
-    public ObjectMapper objectMapper() {
-        ObjectMapper mapper = new ObjectMapper();
-        mapper.registerModule(new JodaModule()); // Apenas para Joda DateTime
-        return mapper;
+    public JodaModule jodaModule() {
+        return new JodaModule();
     }
-} 
+}
